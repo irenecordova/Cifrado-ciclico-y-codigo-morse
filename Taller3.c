3a2bb1c8aa50c40d890247f6de4649bcd9bcb3f3
Irene Cordova <irene_cordova@outlook.es>
2017-05-21 14:04:59 -0500

    creacion de la funcion

--- Taller3.c
+++ Taller3.c
@@ -1,1 +1,32 @@
-#cifrado ciclico
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+int cifradoCiclico(char cadena[],  int llave)
+{
+
+
+        for (int i=0; i<strlen(cadena); i++)
+        {
+          char caracter=cadena[i];
+          if  ispunct(caracter)
+                caracter=caracter;
+          else if isspace(caracter)
+                caracter=caracter;
+          else if  (caracter == 'Z')
+                caracter=caracter-26+llave;
+          else
+                caracter=caracter+llave;
+          printf("%c",caracter);
+        }
+}
+
+
+void main()
+{
+  char cadena[]= "ZOILA";
+  cifradoCiclico(cadena, 3);
+}
+
+
+
